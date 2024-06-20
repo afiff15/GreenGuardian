a9fbb53402ef9f5a1f3b2a1ae8b2c0eaa32fe862
Rafat <rafatreswara33@gmail.com>
2024-06-21 01:09:23 +0700

    fix some issue when upload predictions

--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,10 +1,27 @@
-const { Firestore } = require('@google-cloud/firestore');
+const { initializeApp } = require('firebase/app');
+const { getFirestore, doc, setDoc } = require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYjEcwrTOBQephvwmXvcINvUh7mZl20LU",
+  authDomain: "greenguardian-426110.firebaseapp.com",
+  projectId: "greenguardian-426110",
+  storageBucket: "greenguardian-426110.appspot.com",
+  messagingSenderId: "298148258574",
+  appId: "1:298148258574:web:802c24924ce1ba2edaed5f",
+  measurementId: "G-E9XYPHKGGD"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 async function storeData(id, data) {
-  const db = new Firestore();
-
-  const predictCollection = db.collection('predictions');
-  return predictCollection.doc(id).set(data);
+  try {
+    const predictDocRef = doc(db, 'predictions', id);
+    await setDoc(predictDocRef, data);
+    console.log(`Document ${id} written successfully`);
+  } catch (error) {
+    console.error("Error writing document: ", error);
+  }
 }
 
 module.exports = storeData;
